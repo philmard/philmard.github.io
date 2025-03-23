a76dedb695a554abbb7531fc6cc83aef0edd190c
philmard <philipmardir@gmail.com>
2025-03-23 23:31:54 +0200

    minor ui changes

--- scripts/dark-mode-toggle.js
+++ scripts/dark-mode-toggle.js
@@ -7,7 +7,7 @@ function toggleDarkMode() {
   // Save the current theme to localStorage
   if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("theme", "dark");
-    button.textContent = "☀️"; // Sun emoji when dark mode is on
+    // button.textContent = "☀️"; // Sun emoji when dark mode is on
   } else {
     localStorage.setItem("theme", "light");
     button.textContent = "☾"; // Moon emoji when light mode is on
