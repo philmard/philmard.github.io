80ff6013ab7c630c2925c728a070288393895bb8
philmard <philipmardir@gmail.com>
2025-03-25 23:34:52 +0200

    Removed dark mode

--- scripts/dark-mode-toggle.js
+++ scripts/dark-mode-toggle.js
@@ -1,15 +1,15 @@
-// Function to toggle dark mode
-function toggleDarkMode() {
-  const button = document.querySelector(".toggle-btn");
+// // Function to toggle dark mode
+// function toggleDarkMode() {
+//   const button = document.querySelector(".toggle-btn");
 
-  document.body.classList.toggle("dark-mode");
+//   document.body.classList.toggle("dark-mode");
 
-  // Save the current theme to localStorage
-  if (document.body.classList.contains("dark-mode")) {
-    localStorage.setItem("theme", "dark");
-    // button.textContent = "☀️"; // Sun emoji when dark mode is on
-  } else {
-    localStorage.setItem("theme", "light");
-    button.textContent = "☾"; // Moon emoji when light mode is on
-  }
-}
+//   // Save the current theme to localStorage
+//   if (document.body.classList.contains("dark-mode")) {
+//     localStorage.setItem("theme", "dark");
+//     // button.textContent = "☀️"; // Sun emoji when dark mode is on
+//   } else {
+//     localStorage.setItem("theme", "light");
+//     button.textContent = "☾"; // Moon emoji when light mode is on
+//   }
+// }
